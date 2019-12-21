261059f83b5def77ece76b397159e5fbdd41ed5c
Peter <peter.rootham@alofmethbin.com>
2019-12-21 22:15:06 +0000

    Output errors to a HTML file for convenience in manually checking

--- check_links.rb
+++ check_links.rb
@@ -11,88 +11,48 @@ require 'cgi'
 class LinkChecker
 	ARTICLES='/Users/peter/Website/articles'
 	DATABASE='/Users/peter/Website/data/website.sqlite'
+	LAST_CHECKED='/Users/peter/Website/data/last_checked.yaml'
 	TABLES={'dramas'=>['name','url'],
 			'films'=>['name','url']}
-	MAX_ERRS = 30
+	MAX_ERRS = 5
 	
 	def initialize
 		@links = Hash.new {|h,k| h[k] = []}
-    @info  = Hash.new {|h,k| h[k] = {}}
+		@last_checked = {}
+		@problems = {}
+
+		if File.exist?( LAST_CHECKED)
+			@last_checked = YAML.load( IO.read( LAST_CHECKED))
+		end
 	end
 
-	def check_link( url, info)
-    escaped = CGI::escapeHTML( url)
-		#return "#{url}: Bad characters" if /["'`' ]/ =~ url
-		#puts "check_link: #{url}"
-		headers = `curl -s -I '#{escaped}'`.split("\n")
-		if headers.size == 0
-			return nil if info['no_headers']
-			return "#{url}: No headers"
-		end
-		
-		code = headers[0].split(' ')[1]
-    expected_responses = info['response'] ? info['response'].to_s.split(' ') : []
-		return nil if expected_responses.include?( code)
-		
-		if ['301', '302'].include?( code)
-			location = '???'
-			headers[1..-1].each do |header|
-				if m = /^Location: (.*)$/.match( header.chomp)
-					location = m[1]
-				end
-			end
-			
-			return nil if /^\// =~ location
-			return nil if /^\?/ =~ location
-			return nil if location[0..url.size] == (url + '?')
-			return nil if location == info['redirect']
-      return nil if location == url
-			return "#{url}\n    moved to\n    #{location}\n"
-		end
-
-		if not ['200','302','503'].include?( code)
-			return "#{url}\n    response code [#{code}]\n"
-		end
-		
-		nil
-	end
-	
 	def check_links
-		n_errs = 0
-		
-		YAML.load( File.open( ARTICLES + "/links.yaml")).each_pair do |link, info|
-      @info[info['url']] = info
-			error = check_link( info['url'], info)
-			
-			if not error.nil?
-				n_errs += 1
-				puts '***** ' + link + "\n    " + error
-			end
-		end
-		
-		@links.each_pair do |url, origins|
-      error = nil
-      begin
-			  error = check_link( url, @info[url])
-      rescue Exception => bang
-        p [url, @info[url]]
-        error = bang.message
-      end
-			
-			if not error.nil?
-				n_errs += 1
-				origins.each {|o| puts "***** #{o}"}
-				puts '    ' + error
-			end
-			
-			if n_errs > MAX_ERRS
-				puts "*** More than #{MAX_ERRS} errors"
-				exit( 1)
+		@links.keys.each do |url|
+			escaped = CGI::escapeHTML( url)
+			#return "#{url}: Bad characters" if /["'`' ]/ =~ url
+			#puts "check_link: #{url}"
+			response = `curl -s -I '#{escaped}'`
+			response.force_encoding( 'UTF-8')
+			response.encode!( 'US-ASCII',
+											  :invalid => :replace, :undef => :replace, :universal_newline => true)
+			headers = response.split("\n")
+
+			if headers.size == 0
+				@problems[url] = 'No headers'
+			else
+				code = headers[0].split(' ')[1]
+				if not ['200'].include?( code)
+					@problems[url] = "Response code [#{code}]\n"
+				end
 			end
 		end
 	end
 	
 	def find_links
+		YAML.load( File.open( ARTICLES + "/links.yaml")).each_pair do |link, info|
+			@links[info['url']] << "File: #{ARTICLES}/links.yaml, Name: #{link}"
+		end
+
 		scan_articles( ARTICLES) do |f|
 			lines = IO.readlines( f)
 			lines.each_index do |i|
@@ -113,7 +73,39 @@ class LinkChecker
 			end
 		end
 	end
-	
+
+	def report_errors( path)
+		now = Time.now.to_i
+		sorted = @problems.keys.sort_by do |url|
+			@last_checked[url] ? @last_checked[url] : now
+		end
+
+		if sorted.size > MAX_ERRS
+			sorted = sorted[0...MAX_ERRS]
+		end
+
+		File.open( path, 'w') do |io|
+			io.puts "<html><body><center><table border=\"1\" cellpadding=\"2\">"
+			sorted.each do |url|
+				io.puts "<tr><td><a href=\"#{url}\">#{url}</a></td>"
+				io.puts "<td>#{@problems[url]}</td>"
+				@last_checked[url] = now
+				io.puts "<td>#{@links[url].join('<br>')}</td></tr>"
+			end
+			io.puts "</table></center></body></html>"
+		end
+
+		if sorted.size > 0
+			puts "\n#{@problems.size} errors in all"
+		end
+	end
+
+	def save_last_checked
+		File.open( LAST_CHECKED, 'w') do |io|
+			io.print @last_checked.to_yaml
+		end
+	end
+
 	def scan_articles( dir)
 		if File.directory?( dir)
 			Dir.entries( dir).each do |f|
@@ -131,4 +123,5 @@ lc = LinkChecker.new
 #raise 'Testing'
 lc.find_links
 lc.check_links
-
+lc.report_errors( ARGV[0])
+lc.save_last_checked
